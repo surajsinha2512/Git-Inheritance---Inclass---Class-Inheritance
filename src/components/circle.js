b1f0bb1dd5b547524d5fa9900bb8d350c88ce9b1
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-10-29 16:29:19 +0530

    added shape and circle

--- src/components/circle.js
+++ src/components/circle.js
@@ -4,3 +4,14 @@
 
 
 // export class using module.exports
+
+import shape from './shape';
+export default class Circle extends Shape {
+    constructor(){
+        super();
+    }
+    calculateArea(){
+        console.log ("Area of the circle is"+this.color);
+      return 3.14*5*5;
+    }
+}
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,17 @@
 // declare class
 
 // export class using module.exports
+class shape {
+    constructor(){
+        this.color="red";
+
+    }
+    drawShape(){
+        console.log("draw shape");
+    }
+    calculateArea(){
+        console.log("dont know the area");
+        return 0;
+    }
+}
+export {sgape}
